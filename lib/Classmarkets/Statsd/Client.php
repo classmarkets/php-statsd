030fb1a7eb6709b907008c013178585bbd205c75
Peter Schultz <peter.schultz@classmarkets.com>
2012-11-05 20:21:04 +0100

    using single socket per client instance

--- lib/Classmarkets/Statsd/Client.php
+++ lib/Classmarkets/Statsd/Client.php
@@ -5,19 +5,25 @@ class Client implements ClientInterface
 {
     private $host;
     private $port;
+    private $socket;
 
-    public function __construct($host = 'localhost', $port = 8125)
+    private $messageLength;
+
+    public function __construct($host = 'localhost', $port = 8125, $socket = null)
     {
         $this->host = $host;
         $this->port = $port;
+
+        $this->socket = $socket ?: socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+        $this->messageLength = extension_loaded('mbstring') ? 'mb_strlen' : 'strlen';
     }
 
     public function send($value, $rate)
     {
-        $fp = fsockopen('udp://' . $this->host, $this->port, $errno, $errstr);
-        if (false !== $fp) {
-            fwrite($fp, "$value|@$rate");
-            fclose($fp);
-        }
+        $message = sprintf("%s|@%f", $value, $rate);
+        $messageLength = call_user_func($this->messageLength, $message);
+
+        return socket_sendto($this->socket, $message, $messageLength, 0, $this->host, $this->port);
     }
 }
